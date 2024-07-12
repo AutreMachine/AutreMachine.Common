999f3ef84aadb85d85f56f08b4de42c2b31c9b15
AutreMachine <admin@autremachine.com>
2024-07-12 10:58:38 +0200

    Adding a Samples Razor test project

---
+++ AutreMachine.Common.Razor.Samples/AutreMachine.Common.Razor.Samples.Client/Pages/BaseClass.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutreMachine.Common.Razor.Samples
+{
+    public abstract class BaseClass : ITableCRUDId //, ICloneable
+    {
+        public int Id { get; set; }
+        public DateTime DateCreated { get; set; }
+        
+    }
+}
